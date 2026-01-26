b477006137e0fc5f4b5e9e8c9ce8e0d8aa9944a6
Anshu Pathak <anshuapr@gmail.com>
2026-01-25 19:33:56 -0500

    feat: organized the api to focus solely on data fetching

--- api/index.ts
+++ api/index.ts
@@ -1,95 +1,45 @@
-import { Province, District, Constituency, Candidate } from '../src/types';
-
-export async function fetch_provinces(): Promise<Province[]> {
+export async function fetchProvinces() {
   const res = await fetch(
     'https://result.election.gov.np/JSONFiles/JSONMap/geojson/Province.json'
   );
   const data = await res.json();
-
-  return data.features.map((p: any) => ({
-    id: p['properties']['STATE_C'],
-    name_en: null,
-    name_np: p['properties']['STATE_N'],
-    coordinates: p['geometry']['coordinates'],
-  }));
+  return data;
 }
 
-export async function fetch_districts(provinces: Province[]): Promise<District[]> {
-  
-  const districts = [];
-  for (let province of provinces) {
-    const res = await fetch(
-      `https://result.election.gov.np/JSONFiles/JSONMap/geojson/District/STATE_C_${province.id}.json`
-    );
-    const data = await res.json();
-
-    districts.push(
-      ...data.features.map((d: any) => ({
-        id: d['properties']['DCODE'],
-        province: d['properties']['STATE_C'],
-        name_en: d['properties']['DISTRICT_N'],
-        name_np: d['properties']['DISTRICT_NP'],
-        coordinates: d['geometry']['coordinates'],
-      }))
-    );
-  }
-  return districts;
+export async function fetchDistricts(provinceId: number) {
+  const res = await fetch(
+    `https://result.election.gov.np/JSONFiles/JSONMap/geojson/District/STATE_C_${provinceId}.json`
+  );
+  const data = await res.json();
+  return data;
 }
 
-export async function fetch_constituencies(
-  districts: District[]
-): Promise<Constituency[]> {
-  
-  const constituencies = [];
-  for (let district of districts) {
-    const res = await fetch(
-      `https://result.election.gov.np/JSONFiles/JSONMap/geojson/Const/dist-${district.id}.json`
-    );
-
-    const data = await res.json();
-
-    constituencies.push(
-      ...data.features.map((c: any) => ({
-        id: c['properties']['F_CONST'],
-        district: district.id,
-        province: district.province,
-        coordinates: c['geometry']['coordinates'],
-      }))
-    );
-  }
-  return constituencies;
+export async function fetchConstituencies(districtId: number) {
+  const res = await fetch(
+    `https://result.election.gov.np/JSONFiles/JSONMap/geojson/Const/dist-${districtId}.json`
+  );
+  const data = await res.json();
+  return data;
 }
 
-export async function fetch_HOR_candidates(
-  constituencies: Constituency[]
-): Promise<Candidate[]> {
-  
-  const candidates = [];
-  for (let constituency of constituencies) {
-    const res = await fetch(
-      `https://result.election.gov.np/JSONFiles/Election2079/HOR/FPTP/HOR-${constituency.district}-${constituency.id}.json`
-    );
-    const data = await res.json();
+// This should be used as the primary function to fetch all candidate data
+export async function fetchAllCandidates() {
+  const res = await fetch(
+    'https://result.election.gov.np/JSONFiles/ElectionResultCentral2082.txt'
+  );
+  const data = await res.json();
+  return data;
+}
 
-    candidates.push(
-      ...data.map((c: any) => ({
-        id: c['CandidateID'],
-        name_en: null,
-        name_np: c['CandidateName'],
-        age: c['Age'],
-        party: c['PoliticalPartyName'],
-        gender: c['Gender'],
-        qualification: c['QUALIFICATION'],
-        remarks: c['REMARKS'],
-        votes: c['TotalVotesReceived'],
-        constituency: c['SCConstID'],
-        district: constituency.district,
-        province: constituency.province,
-        symbol_id: c['SymbolID'],
-        citizenship_district: c['CTZDIST'],
-        experience: c['EXPERIENCE'] 
-      }))
-    );
-  }
-  return candidates;
+// This is only used as fallback if fetchAllCandidates turn out to not work
+// to avoid sending more requests than necessary
+export async function fetchConstituencyCandidates(
+  districtId: number,
+  constituencyId: number
+) {
+  const res = await fetch(
+    `https://result.election.gov.np/JSONFiles/Election2079/HOR/FPTP/HOR-${districtId}-${constituencyId}.json`
+  );
+  const data = await res.json();
+  return data;
 }
