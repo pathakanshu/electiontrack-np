a198cff5f45aae9d53d96f328bdec13197242a88
Anshu Pathak <anshuapr@gmail.com>
2026-01-26 00:49:34 -0500

    feat: created relavant types

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,29 @@
+import { Position } from 'geojson';
+
 export type Province = {
-  id: number;
+  province_id: number;
   name_en: string | null;
   name_np: string;
-  coordinates: number[][][];
+  district_ids: number[];
+  geometry_coords: Position[][][];
 };
 
 export type District = {
-  id: number;
-  province: number;
+  district_id: number;
+  province_id: number;
   name_en: string | null;
   name_np: string;
-  coordinates: number[][][];
+  geometry_coords: Position[][][];
+  constituency_ids: string[];
 };
 
 export type Constituency = {
-  id: number;
-  district: number;
-  province: number;
-  coordinates: number[][][];
+  constituency_id: string;
+  district_id: number;
+  sub_id: number;
+  province_id: number;
+  conservation_area: boolean;
+  geometry_coords: Position[][][];
 };
 
 export type Candidate = {
@@ -34,7 +40,7 @@ export type Candidate = {
   district: number;
   province: number;
   symbol_id: number;
-  citizenship_district: string;
+  experience: string;
 };
 
 export type ElectionData = {
@@ -42,4 +48,62 @@ export type ElectionData = {
   districts: District[];
   constituencies: Constituency[];
   candidates: Candidate[];
+};
+
+export type ProvinceFeature = {
+  type: string;
+  properties: {
+    STATE_C: number;
+    STATE_N: string;
+    Country: string;
+    Country_N: string;
+  };
+  geometry: {
+    type: string;
+    coordinates: Position[][][];
+  };
+};
+
+export type DistrictFeature = {
+  type: string;
+  properties: {
+    STATE_C: number;
+    STATE_N: string;
+    DCODE: number;
+    DISTRICT: string;
+    DISTRICT_N: string;
+  };
+  geometry: {
+    type: string;
+    coordinates: Position[][][];
+  };
+};
+
+export type ConstituencyFeature = {
+  type: string;
+  properties: {
+    OBJECTID: number;
+    STATE_C: number;
+    STATE_N: string;
+    DCODE: number;
+    DISTRICT: string;
+    DISTRICT_N: string;
+    F_CONST: number;
+    Conservati: string | null;
+  };
+  geometry: {
+    type: string;
+    coordinates: Position[][][];
+  };
+};
+
+export type DistrictIdentifier = {
+  id: number;
+  name: string;
+  parentId: number;
+};
+
+export type ConstituencyIdentifier = {
+  distId: number;
+  consts: number;
 };
